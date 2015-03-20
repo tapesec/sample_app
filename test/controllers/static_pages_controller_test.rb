004eecf7585ab85a24c8a679c201cae0f3f0b7a7
tapesec <lionel.dupouy@gmail.com>
2015-03-20 17:01:31 +0100

    Mdification du yield

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutoriel Sample App"
+    assert_select "title", "Home | Ruby on Rails Tutoriel Sample App Home Page"
   end
 
   test "should get help" do
